ff6104c40531ca97dc219cadec99e2070edcd53b
NihadVeliyev <nihadv795@gmail.com>
2025-07-10 21:49:26 +0400

    Only True or False task done!

---
+++ hyper-skills-projects/src/main/java/Projects/FalseOrTrue.java
@@ -0,0 +1,15 @@
+package Projects;
+
+public class FalseOrTrue {
+    public static void main(String[] args) {
+
+    }
+    public boolean toPrimitive(Boolean object){
+        if (object == null) {
+            return false;
+        }
+        return object;
+    }
+
+
+}
