3b5fd1d85a2ee4b9552450b2655e190af951a29f
NihadVeliyev <nihadv795@gmail.com>
2025-07-16 03:12:43 +0400

    Add ReturnEmptyString class with toUpperCase method

---
+++ hyper-skills-projects/src/main/java/Projects/ReturnEmptyString.java
@@ -0,0 +1,18 @@
+package Projects;
+
+public class ReturnEmptyString {
+    public static void main(String[] args) {
+
+    }
+    public static String toUpperCase(String str) {
+
+        if(str==null){
+            return "";
+        }else {
+            return str.toUpperCase();
+
+        }
+
+    }
+
+}
