1993218562071ab0717609ef95c86f4d561e029a
NihadVeliyev <nihadv795@gmail.com>
2025-07-19 20:59:51 +0400

    Add MainSummingThreads and RangeSummator classes for concurrent summation of integer ranges

---
+++ hyper-skills-projects/src/main/java/Projects/SummingThreads/RangeSummator.java
@@ -0,0 +1,31 @@
+package Projects.SummingThreads;
+
+ class RangeSummator extends Thread {
+
+    int fromIncl;
+    int toIncl;
+
+    private volatile long result = 0;
+
+    public RangeSummator(int fromIncl, int toIncl) {
+        this.fromIncl = fromIncl;
+        this.toIncl = toIncl;
+    }
+
+    @Override
+    public void run() {
+        final long currentId = Thread.currentThread().getId();
+
+        long sum = 0;
+        for (int i = fromIncl; i <= toIncl; i++) {
+            sum += i;
+        }
+
+        result = sum;
+    }
+
+    public long getResult() {
+        return result;
+    }
+}
+}
