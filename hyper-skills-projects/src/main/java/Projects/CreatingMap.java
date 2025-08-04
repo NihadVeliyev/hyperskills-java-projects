73e2bc09cadfb9224a3a45266cc8ca8845eeb442
NihadVeliyev <nihadv795@gmail.com>
2025-08-04 16:27:18 +0400

    Add CreatingMap class to demonstrate usage of TreeMap for storing key-value pairs

---
+++ hyper-skills-projects/src/main/java/Projects/CreatingMap.java
@@ -0,0 +1,14 @@
+package Projects;
+
+import java.util.TreeMap;
+
+public class CreatingMap {
+    public static void main(String[] args) {
+
+        TreeMap<String,Integer> values=new TreeMap<>();
+        values.put("Alice",6);
+        values.put("Nihad",2);
+        values.put("Noro",10);
+        System.out.println(values);
+    }
+}
