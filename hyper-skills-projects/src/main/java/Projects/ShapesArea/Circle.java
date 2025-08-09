f34fdebd4e97ff0a11af479272462d9cc670d048
NihadVeliyev <nihadv795@gmail.com>
2025-08-09 15:41:28 +0400

    Add Circle class with constructor and area method

--- hyper-skills-projects/src/main/java/Projects/ShapesArea/Circle.java
+++ hyper-skills-projects/src/main/java/Projects/ShapesArea/Circle.java
@@ -1,4 +1,14 @@
 package Projects.ShapesArea;
 
 public class Circle extends Shape{
+    double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return radius*radius*Math.PI;
+    }
 }
