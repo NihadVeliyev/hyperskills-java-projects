dcc7eb2fda3ea9f2e4aa47deb652d05a6f7b2266
NihadVeliyev <nihadv795@gmail.com>
2025-08-09 15:35:06 +0400

    Add Triangle class with constructor and area method

--- hyper-skills-projects/src/main/java/Projects/ShapesArea/Triangle.java
+++ hyper-skills-projects/src/main/java/Projects/ShapesArea/Triangle.java
@@ -1,4 +1,17 @@
 package Projects.ShapesArea;
 
 public class Triangle extends Shape{
+    double height;
+    double base;
+
+    public Triangle(double height,double base){
+        this.base=base;
+        this.height=height;
+    }
+
+    @Override
+    public double area() {
+
+        return height*base/2 ;
+    }
 }
