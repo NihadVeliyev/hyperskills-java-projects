8315be8a99c2f9057a68abbb06b52cdc27576c4b
NihadVeliyev <nihadv795@gmail.com>
2025-08-09 15:37:09 +0400

    Add Rectangle class with constructor and area method

--- hyper-skills-projects/src/main/java/Projects/ShapesArea/Rectangle.java
+++ hyper-skills-projects/src/main/java/Projects/ShapesArea/Rectangle.java
@@ -1,4 +1,16 @@
 package Projects.ShapesArea;
 
 public class Rectangle extends Shape{
+    double width;
+    double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double area() {
+        return width*height;
+    }
 }
