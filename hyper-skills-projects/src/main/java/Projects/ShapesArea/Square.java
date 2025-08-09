373232ab134950a00d1f7f974a739c3afa9f7a46
NihadVeliyev <nihadv795@gmail.com>
2025-08-09 15:32:40 +0400

    Add area method to Square class and implement constructor for side length

--- hyper-skills-projects/src/main/java/Projects/ShapesArea/Square.java
+++ hyper-skills-projects/src/main/java/Projects/ShapesArea/Square.java
@@ -1,4 +1,15 @@
 package Projects.ShapesArea;
 
 public class Square extends Shape{
+    double side;
+
+    @Override
+    public double area(){
+        return side*side;
+
+    }
+
+    public Square(double side) {
+        this.side = side;
+    }
 }
