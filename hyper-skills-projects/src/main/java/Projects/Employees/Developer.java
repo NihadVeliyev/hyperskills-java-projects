29f44cd0645b2faff386208ce05d84400f40d4f0
NihadVeliyev <nihadv795@gmail.com>
2025-08-11 00:59:08 +0400

    Add mainLanguage and skills properties to Developer class with constructor

--- hyper-skills-projects/src/main/java/Projects/Employees/Developer.java
+++ hyper-skills-projects/src/main/java/Projects/Employees/Developer.java
@@ -1,4 +1,12 @@
 package Projects.Employees;
 
-public class Developer extends Employee{
+public class Developer extends Employee {
+    String mainLanguage;
+    String[] skills;
+
+    public Developer(String name, String email, String experience, String mainLanguage, String[] skills) {
+        super(name, email, experience);
+        this.mainLanguage = mainLanguage;
+        this.skills = skills;
+    }
 }
