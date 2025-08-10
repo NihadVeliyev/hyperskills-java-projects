f00f0d276b7f05df7672c2eecb6c5641da65d613
NihadVeliyev <nihadv795@gmail.com>
2025-08-11 00:57:52 +0400

    Add properties and methods to Employee class for name, email, and experience

--- hyper-skills-projects/src/main/java/Projects/Employees/Employee.java
+++ hyper-skills-projects/src/main/java/Projects/Employees/Employee.java
@@ -1,4 +1,37 @@
 package Projects.Employees;
 
 public class Employee {
+    String name;
+    String email;
+    String experience;
+
+    public Employee(String name, String email, String experience) {
+        this.name = name;
+        this.email = email;
+        this.experience = experience;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getExperience() {
+        return experience;
+    }
+
+    public void setExperience(String experience) {
+        this.experience = experience;
+    }
 }
