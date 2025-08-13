3b9c43abea0735b79472190bc7d9e92f40a4cf8d
NihadVeliyev <nihadv795@gmail.com>
2025-08-13 17:27:41 +0400

    Add Meal abstract class and Steak implementation for meal preparation

---
+++ hyper-skills-projects/src/main/java/Projects/Template/Meal.java
@@ -0,0 +1,24 @@
+package Projects.Template;
+
+abstract class Meal {
+
+    /**
+     * It provides template method of meal routine.
+     */
+    public void doMeal() {
+        // write your code here ...
+        this.prepareIngredients();
+        this.cook();
+        this.eat();
+        this.cleanUp();
+    }
+
+    public abstract void prepareIngredients();
+    public abstract void cook();
+
+    public void eat() {
+        System.out.println("That's good");
+    }
+
+    public abstract void cleanUp();
+}
