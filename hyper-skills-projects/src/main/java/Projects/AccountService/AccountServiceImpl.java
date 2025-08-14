035a9776b79cd263b0019b51600a98b259c83253
NihadVeliyev <nihadv795@gmail.com>
2025-08-14 18:41:08 +0400

    Implement AccountServiceImpl with methods for account retrieval and balance counting

--- hyper-skills-projects/src/main/java/Projects/AccountService/AccountServiceImpl.java
+++ hyper-skills-projects/src/main/java/Projects/AccountService/AccountServiceImpl.java
@@ -1,4 +1,31 @@
 package Projects.AccountService;
 
-public class AccountServiceImpl {
+class AccountServiceImpl implements AccountService {
+    // TODO: store the array here (consider making it private final)
+    private /* final */ Account[] accounts;
+
+    // TODO: constructor that accepts Account[] and stores it
+    AccountServiceImpl(Account[] accounts) {
+        // TODO: assign the field
+        this.accounts = accounts;
+    }
+
+    @Override
+    public Account findAccountByOwnerId(long id) {
+        // TODO:
+        // loop over accounts
+        // if account.getOwner().getId() == id -> return that account
+        // if none found -> return null
+        return null; // placeholder
+    }
+
+    @Override
+    public long countAccountsWithBalanceGreaterThan(long value) {
+        // TODO:
+        // long count = 0;
+        // loop over accounts
+        // if account.getBalance() > value -> count++
+        // return count
+        return 0L; // placeholder
+    }
 }
