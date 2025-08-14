78e716b9cbda6e89c17f3e88360067bdc191256c
NihadVeliyev <nihadv795@gmail.com>
2025-08-14 18:46:45 +0400

    Refactor AccountServiceImpl to finalize account array storage and implement account retrieval by owner ID

--- hyper-skills-projects/src/main/java/Projects/AccountService/AccountServiceImpl.java
+++ hyper-skills-projects/src/main/java/Projects/AccountService/AccountServiceImpl.java
@@ -1,31 +1,25 @@
 package Projects.AccountService;
 
 class AccountServiceImpl implements AccountService {
-    // TODO: store the array here (consider making it private final)
-    private /* final */ Account[] accounts;
+    private final Account[] accounts;
 
-    // TODO: constructor that accepts Account[] and stores it
     AccountServiceImpl(Account[] accounts) {
-        // TODO: assign the field
         this.accounts = accounts;
     }
 
     @Override
     public Account findAccountByOwnerId(long id) {
-        // TODO:
-        // loop over accounts
-        // if account.getOwner().getId() == id -> return that account
-        // if none found -> return null
-        return null; // placeholder
+        for(Account account:accounts){
+            if(account.getId()==id){
+                return account;
+            }
+        }
+        return null;
     }
 
     @Override
     public long countAccountsWithBalanceGreaterThan(long value) {
-        // TODO:
-        // long count = 0;
-        // loop over accounts
-        // if account.getBalance() > value -> count++
-        // return count
-        return 0L; // placeholder
+
+        return 0L;
     }
 }
