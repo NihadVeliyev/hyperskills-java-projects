4b8bae44e8d8c561016f7ad2e346efbd1b654c49
NihadVeliyev <nihadv795@gmail.com>
2025-08-15 14:57:07 +0400

    Add static fields and lambda expression to Operator class

--- hyper-skills-projects/src/main/java/Projects/Operator.java
+++ hyper-skills-projects/src/main/java/Projects/Operator.java
@@ -4,4 +4,9 @@ import java.util.function.DoubleUnaryOperator;
 
 class Operator {
 
+    public static int a = 10;
+    public static int b = 20;
+    public static int c = 30;
+
+    public static DoubleUnaryOperator unaryOperator = x->a*x*x+b*x+c;// your lambda here;
 }
