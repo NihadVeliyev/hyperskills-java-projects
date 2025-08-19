21588b3521cb5d246d363e558fb18746f7006926
NihadVeliyev <nihadv795@gmail.com>
2025-08-19 15:44:41 +0400

    Remove TODO comments from run method in CounterThread

--- hyper-skills-projects/src/main/java/Projects/CounterThread.java
+++ hyper-skills-projects/src/main/java/Projects/CounterThread.java
@@ -6,9 +6,6 @@ class CounterThread extends Thread {
     public void run() {
         long counter = 0;
         while (true) {
-            // TODO: check if the thread was interrupted
-            // If yes -> print "It was interrupted" and stop the loop
-            // Otherwise, keep incrementing the counter
             if(isInterrupted()){
                 System.out.println("It was interrupted");
                 break;
