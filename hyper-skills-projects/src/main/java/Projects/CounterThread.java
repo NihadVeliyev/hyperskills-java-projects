f7a996f7721776f6b85555b1101a357da83b321a
NihadVeliyev <nihadv795@gmail.com>
2025-08-19 15:44:23 +0400

    Implement run method in CounterThread to handle interruption and counter increment

--- hyper-skills-projects/src/main/java/Projects/CounterThread.java
+++ hyper-skills-projects/src/main/java/Projects/CounterThread.java
@@ -2,6 +2,19 @@ package Projects;
 
 class CounterThread extends Thread {
 
-
+    @Override
+    public void run() {
+        long counter = 0;
+        while (true) {
+            // TODO: check if the thread was interrupted
+            // If yes -> print "It was interrupted" and stop the loop
+            // Otherwise, keep incrementing the counter
+            if(isInterrupted()){
+                System.out.println("It was interrupted");
+                break;
+            }
+            counter++;
+        }
+    }
 }
 
